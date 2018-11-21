b3a4c81a2a540def79a33a45d7326c463b95f1c5
Aust95 <augustorabreu95@gmail.com>
2018-11-20 22:41:25 -0200

    commit

--- src/rom.c
+++ src/rom.c
@@ -3,4 +3,13 @@
 void load_rom(void)
 {
 	printf("LOAD ROM: TODO IMPLEMENTATION\n");
+
+	FILE *fp;
+	int MAX_FILESIZE = 512;
+	char data[MAX_FILESIZE];
+	fp = fopen("rom.ch8", "r");
+	if(fp != NULL){
+		fgets(data, MAX_FILESIZE, fp);
+		printf("%s\n",data);
+	}	
 }
